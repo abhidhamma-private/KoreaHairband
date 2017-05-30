e841e635b5f2a68b55ecb3892ad958418139c5c8
user1 <user1@user1-PC>
2017-05-30 16:44:34 +0900

    회원쪽 수정 및 header.jsp/MyUtil.java 파일 변경

--- semiproject/src/com/util/MyUtil.java
+++ semiproject/src/com/util/MyUtil.java
@@ -20,7 +20,7 @@ public class MyUtil {
 	}
 	
     //********************************************
-	// 페이징(paging) 처리(GET 방식) : 부트스트랩
+	// 페이징(paging) 처리(GET 방식)
 	public String paging(int current_page, int total_page, String list_url) {
 		StringBuffer sb=new StringBuffer();
 		
@@ -41,176 +41,155 @@ public class MyUtil {
 		if(current_page%numPerBlock==0)
 			currentPageSetup=currentPageSetup-numPerBlock;
 
-		sb.append("<ul class='pagination pagination-sm'>");
-		// 처음페이지
-		if(current_page > 1) {
-			sb.append("<li><a href='"+list_url+"page=1' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		}
-		// 이전(10페이지 전)
+		sb.append("<style type='text/css'>");
+		sb.append("#paginate {clear:both;font:12px \"맑은 고딕\",NanumGothic,돋움,Dotum,AppleGothic;padding:15px 0px 0px 0px;text-align:center;height:28px;white-space:nowrap;}");
+		sb.append("#paginate a {border:1px solid #ccc;height:28px;color:#000000;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;outline:none; select-dummy: expression(this.hideFocus=true);}");
+		sb.append("#paginate a:hover, a:active {border:1px solid #ccc;color:#6771ff;vertical-align:middle; line-height:normal;}");
+		sb.append("#paginate .curBox {border:1px solid #e28d8d; background: #fff; color:#cb3536; font-weight:bold;height:28px;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("#paginate .numBox {border:1px solid #ccc;height:28px;font-weight:bold;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("</style>");
+		
+		sb.append("<div id='paginate'>");
+		// 처음페이지, 이전(10페이지 전)
 		n=current_page-numPerBlock;
 		if(total_page > numPerBlock && currentPageSetup > 0) {
-			sb.append("<li><a href='"+list_url+"page="+n+"' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
+			sb.append("<a href='"+list_url+"page=1'>처음</a>");
+			sb.append("<a href='"+list_url+"page="+n+"'>이전</a>");
 		}
 		
 		// 바로가기
 		page=currentPageSetup+1;
 		while(page<=total_page && page <=(currentPageSetup+numPerBlock)) {
 			if(page==current_page) {
-				sb.append("<li class='active'><a href='#'>"+page+" <span class='sr-only'>(current)</span></a></li>");
+				sb.append("<span class='curBox'>"+page+"</span>");
 			} else {
-				sb.append("<li><a href='"+list_url+"page="+page+"'>"+page+"</a></li>");
+				sb.append("<a href='"+list_url+"page="+page+"' class='numBox'>"+page+"</a>");
 			}
 			page++;
 		}
 		
-		// 다음(10페이지 후)
+		// 다음(10페이지 후), 마지막페이지
 		n=current_page+numPerBlock;
 		if(n>total_page) n=total_page;
 		if(total_page-currentPageSetup>numPerBlock) {
-			sb.append("<li><a href='"+list_url+"page="+n+"' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
+			sb.append("<a href='"+list_url+"page="+n+"'>다음</a>");
+			sb.append("<a href='"+list_url+"page="+total_page+"'>끝</a>");
 		}
-		// 마지막페이지
-		if(current_page<total_page) {
-			sb.append("<li><a href='"+list_url+"page="+total_page+"' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		}
-		
-		sb.append("</ul>");
+		sb.append("</div>");
 	
 		return sb.toString();
 	}
 
     //********************************************
-	// javascript 페이지 처리(javascript listPage() 함수 호출) : 부트스트랩
+	// javascript 페이지 처리(javascript listPage() 함수 호출)
     public String paging(int current_page, int total_page) {
 		if(current_page < 1 || total_page < 1)
 			return "";
 
         int numPerBlock = 10;   // 리스트에 나타낼 페이지 수
-        int currentPageSetup;
+        int currentPageSetUp;
         int n;
         int page;
         StringBuffer sb=new StringBuffer();
         
         // 표시할 첫 페이지
-        currentPageSetup = (current_page / numPerBlock) * numPerBlock;
+        currentPageSetUp = (current_page / numPerBlock) * numPerBlock;
         if (current_page % numPerBlock == 0)
-        	currentPageSetup = currentPageSetup - numPerBlock;
+            currentPageSetUp = currentPageSetUp - numPerBlock;
 
-		sb.append("<ul class='pagination pagination-sm'>");
+		sb.append("<style type='text/css'>");
+		sb.append("#paginate {clear:both;font:12px \"맑은 고딕\",NanumGothic,돋움,Dotum,AppleGothic;padding:15px 0px 0px 0px;text-align:center;height:28px;white-space:nowrap;}");
+		sb.append("#paginate a {border:1px solid #ccc;height:28px;color:#000000;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;outline:none; select-dummy: expression(this.hideFocus=true);}");
+		sb.append("#paginate a:hover, a:active {border:1px solid #ccc;color:#6771ff;vertical-align:middle; line-height:normal;}");
+		sb.append("#paginate .curBox {border:1px solid #e28d8d; background: #fff; color:#cb3536; font-weight:bold;height:28px;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("#paginate .numBox {border:1px solid #ccc;height:28px;font-weight:bold;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("</style>");
+		
+		sb.append("<div id='paginate'>");
         
-        // 처음페이지
-		if(current_page > 1) {
-			sb.append("<li><a onclick='listPage(1);' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		}
-        // 이전(10페이지 전)
+        // 처음페이지, 이전(10페이지 전)
         n = current_page - numPerBlock;
-		if(total_page > numPerBlock && currentPageSetup > 0) {
-			sb.append("<li><a onclick='listPage("+n+");' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
-		}
+        if ((total_page > numPerBlock) && (currentPageSetUp > 0)) {
+			sb.append("<a onclick='listPage(1);'>처음</a>");
+			sb.append("<a onclick='listPage("+n+");'>이전</a>");
+        }
 
         // 바로가기 페이지 구현
-		page=currentPageSetup+1;
-		while(page<=total_page && page <=(currentPageSetup+numPerBlock)) {
-			if(page==current_page) {
-				sb.append("<li class='active'><a href='#'>"+page+" <span class='sr-only'>(current)</span></a></li>");
-			} else {
-				sb.append("<li><a onclick='listPage("+page+");'>"+page+"</a></li>");
-			}
-			page++;
-		}
-        
-        // 다음(10페이지 후)
+        page = currentPageSetUp + 1;
+        while((page <= total_page) && (page <= currentPageSetUp + numPerBlock)) {
+           if(page == current_page) {
+        	   sb.append("<span class='curBox'>"+page+"</span>");
+           } else {
+			   sb.append("<a onclick='listPage("+page+");' class='numBox'>"+page+"</a>");
+           }
+           page++;
+        }
+
+        // 다음(10페이지 후), 마지막 페이지
         n = current_page + numPerBlock;
 		if(n>total_page) n=total_page;
-		if(total_page-currentPageSetup>numPerBlock) {
-			sb.append("<li><a onclick='listPage("+n+"); aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
-		}
-        // 마지막 페이지
-		if(current_page<total_page) {
-			sb.append("<li><a onclick='listPage("+total_page+");' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		}
-        
-        sb.append("</ul>");
+        if (total_page - currentPageSetUp > numPerBlock) {
+			sb.append("<a onclick='listPage("+n+");'>다음</a>");
+			sb.append("<a onclick='listPage("+total_page+");'>끝</a>");
+        }
+		sb.append("</div>");
 
         return sb.toString();
     }
 
     //********************************************
-	// javascript 페이지 처리(javascript 지정 함수 호출, methodName:호출할 스크립트 함수명) : 부트스트랩
+	// javascript 페이지 처리(javascript 지정 함수 호출, methodName:호출할 스크립트 함수명)
     public String pagingMethod(int current_page, int total_page, String methodName) {
 		if(current_page < 1 || total_page < 1)
 			return "";
 
         int numPerBlock = 10;   // 리스트에 나타낼 페이지 수
-        int currentPageSetup;
+        int currentPageSetUp;
         int n;
         int page;
         StringBuffer sb=new StringBuffer();
         
         // 표시할 첫 페이지
-        currentPageSetup = (current_page / numPerBlock) * numPerBlock;
+        currentPageSetUp = (current_page / numPerBlock) * numPerBlock;
         if (current_page % numPerBlock == 0)
-        	currentPageSetup = currentPageSetup - numPerBlock;
+            currentPageSetUp = currentPageSetUp - numPerBlock;
 
-		sb.append("<ul class='pagination pagination-sm'>");
+		sb.append("<style type='text/css'>");
+		sb.append("#paginate {clear:both;font:12px \"맑은 고딕\",NanumGothic,돋움,Dotum,AppleGothic;padding:15px 0px 0px 0px;text-align:center;height:28px;white-space:nowrap;}");
+		sb.append("#paginate a {border:1px solid #ccc;height:28px;color:#000000;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;outline:none; select-dummy: expression(this.hideFocus=true);}");
+		sb.append("#paginate a:hover, a:active {border:1px solid #ccc;color:#6771ff;vertical-align:middle; line-height:normal;}");
+		sb.append("#paginate .curBox {border:1px solid #e28d8d; background: #fff; color:#cb3536; font-weight:bold;height:28px;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("#paginate .numBox {border:1px solid #ccc;height:28px;font-weight:bold;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
+		sb.append("</style>");
+		
+		sb.append("<div id='paginate'>");
         
-        // 처음페이지
-		if(current_page > 1) {
-			sb.append("<li><a onclick='"+methodName+"(1);' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='First'><span aria-hidden='true' class='glyphicon glyphicon-step-backward'></span></a></li>");
-		}
-        // 이전(10페이지 전)
+        // 처음페이지, 이전(10페이지 전)
         n = current_page - numPerBlock;
-		if(total_page > numPerBlock && currentPageSetup > 0) {
-			sb.append("<li><a onclick='"+methodName+"("+n+");' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Previous'><span aria-hidden='true' class='glyphicon glyphicon-triangle-left'></span></a></li>");
-		}
+        if ((total_page > numPerBlock) && (currentPageSetUp > 0)) {
+			sb.append("<a onclick='"+methodName+"(1);'>처음</a>");
+			sb.append("<a onclick='"+methodName+"("+n+");'>이전</a>");
+        }
 
         // 바로가기 페이지 구현
-		page=currentPageSetup+1;
-		while(page<=total_page && page <=(currentPageSetup+numPerBlock)) {
-			if(page==current_page) {
-				sb.append("<li class='active'><a href='#'>"+page+" <span class='sr-only'>(current)</span></a></li>");
-			} else {
-				sb.append("<li><a onclick='"+methodName+"("+page+");'>"+page+"</a></li>");
-			}
-			page++;
-		}
-        
-        // 다음(10페이지 후)
+        page = currentPageSetUp + 1;
+        while((page <= total_page) && (page <= currentPageSetUp + numPerBlock)) {
+           if(page == current_page) {
+        	   sb.append("<span class='curBox'>"+page+"</span>");
+           } else {
+			   sb.append("<a onclick='"+methodName+"("+page+");' class='numBox'>"+page+"</a>");
+           }
+           page++;
+        }
+
+        // 다음(10페이지 후), 마지막 페이지
         n = current_page + numPerBlock;
 		if(n>total_page) n=total_page;
-		if(total_page-currentPageSetup>numPerBlock) {
-			sb.append("<li><a onclick='"+methodName+"("+n+"); aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Next'><span aria-hidden='true' class='glyphicon glyphicon-triangle-right'></span></a></li>");
-		}
-        // 마지막 페이지
-		if(current_page<total_page) {
-			sb.append("<li><a onclick='"+methodName+"("+total_page+");' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		} else {
-			sb.append("<li class='disabled'><a href='#' aria-label='Last'><span aria-hidden='true' class='glyphicon glyphicon-step-forward'></span></a></li>");
-		}
-        
-        sb.append("</ul>");
+        if (total_page - currentPageSetUp > numPerBlock) {
+			sb.append("<a onclick='"+methodName+"("+n+");'>다음</a>");
+			sb.append("<a onclick='"+methodName+"("+total_page+");'>끝</a>");
+        }
+		sb.append("</div>");
 
         return sb.toString();
     }
@@ -256,7 +235,7 @@ public class MyUtil {
     	 str=str.replaceAll(">", "&gt;");
     	 str=str.replaceAll("<", "&lt;");
     	 
-    	 str=str.replaceAll(" ", "&nbsp;");
+    	 str=str.replaceAll(" ", "&nbsp;"); // \\s는 엔터도 병경되므로 안됨
     	 str=str.replaceAll("\n", "<br>");
     	 
     	 return str;
