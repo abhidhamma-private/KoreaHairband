d02ed3ee0e99354e5aa83c656055cccaac33a017
user1 <user1@user1-PC>
2017-06-05 10:20:59 +0900

    수정수정~~

--- semiproject/src/com/fashion/FashionServlet.java
+++ semiproject/src/com/fashion/FashionServlet.java
@@ -17,6 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+import com.it.BoardDTO;
+import com.it.BoardReplyDTO;
 import com.member.MemberDAO;
 import com.member.SessionInfo;
 import com.oreilly.servlet.MultipartRequest;
@@ -86,14 +89,41 @@ public class FashionServlet extends MyServlet{
 		updateSubmit1(req, resp);
 	} else if(uri.indexOf("delete2.do")!=-1){
 		delete1(req, resp);
-	
 	} else if(uri.indexOf("insertReply.do")!=-1) {
 		// 댓글 추가
 		insertReply(req, resp);
 	} else if(uri.indexOf("listReply.do")!=-1) {
 		// 댓글 리스트
 		listReply(req, resp);
-	}
+	} else	if (uri.indexOf("Fashion2.do") != -1) {
+		Fashion2_list(req, resp);
+	} else if (uri.indexOf("Fashion2_created.do") != -1) {
+		Fashion2_createdForm(req, resp);
+	} else if (uri.indexOf("Fashion2_created_ok.do") != -1) {
+		Fashion2_createdSubmit(req, resp);
+	} else if (uri.indexOf("Fashion2_article.do") != -1) {
+		Fashion2_article(req, resp);
+	} else if (uri.indexOf("Fashion2_update.do") != -1) {
+		Fashion2_updateForm(req, resp);
+	} else if (uri.indexOf("Fashion2_update_ok.do") != -1) {
+		Fashion2_updateSubmit(req, resp);
+	} else if (uri.indexOf("Fashion2_reply.do") != -1) {
+		Fashion2_replyForm(req, resp);
+	} else if (uri.indexOf("Fashion2_reply_ok.do") != -1) {
+		Fashion2_replySubmit(req, resp);
+	} else if (uri.indexOf("Fashion2_delete.do") != -1) {
+		Fashion2_delete(req, resp);
+	} else if (uri.indexOf("Fashion2_like.do") != -1) {
+		Fashion2_like(req, resp);
+	} else if (uri.indexOf("Fashion2_likecnt.do") != -1) {
+		Fashion2_likecnt(req, resp);
+	} else if(uri.indexOf("Fashion2_insertReply.do")!=-1) {
+		Fashion2_insertReply(req, resp);
+	} else if(uri.indexOf("Fashion2_listReply.do")!=-1) {
+		Fashion2_listReply(req, resp);
+	} else if(uri.indexOf("Fashion2_deleteReply.do")!=-1) {
+		Fashion2_deleteReply(req, resp);
+	} 
 	}
 	
 	protected void list1(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -550,5 +580,485 @@ public class FashionServlet extends MyServlet{
 		resp.setContentType("text/html;charset=utf-8");
 		PrintWriter out=resp.getWriter();
 		out.print(job.toString());
+	}
+	
+	private void Fashion2_list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 게시물 리스트
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+		MyUtil util = new MyUtil();
+
+		String page = req.getParameter("page");
+		int current_page = 1;
+		if (page != null)
+			current_page = Integer.parseInt(page);
+
+		// 검색
+		String searchKey = req.getParameter("searchKey");
+		String searchValue = req.getParameter("searchValue");
+		if (searchKey == null) {
+			searchKey = "subject";
+			searchValue = "";
+		}
+		// GET 방식인 경우 디코딩
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			searchValue = URLDecoder.decode(searchValue, "utf-8");
+		}
+
+		// 전체 데이터 개수
+		int dataCount;
+		if (searchValue.length() == 0)
+			dataCount = dao.dataCount();
+		else
+			dataCount = dao.dataCount(searchKey, searchValue);
+
+		// 전체 페이지 수
+		int rows = 10;
+		int total_page = util.pageCount(rows, dataCount);
+
+		if (current_page > total_page)
+			current_page = total_page;
+
+		// 게시물 가져올 시작과 끝
+		int start = (current_page - 1) * rows + 1;
+		int end = current_page * rows;
+
+		// 게시물 가져오기
+		List<Fashion2DTO> list = null;
+		if (searchValue.length() == 0)
+			list = dao.listFashion2(start, end);
+		else
+			list = dao.listFashion2(start, end, searchKey, searchValue);
+		// 공지글 가져오기
+		List<Fashion2DTO> listNotice=null;
+		//if(current_page==1){ -->공지글이 1페이지만 나오게 해줌
+		listNotice = dao.listNotice();
+		Iterator<Fashion2DTO> itNotice=listNotice.iterator();
+		while (itNotice.hasNext()) {
+			Fashion2DTO dto=itNotice.next();
+			dto.setCreated(dto.getCreated().substring(0, 10));
+		}
+		// new,리스트번호
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		long gap;
+		Date date = new Date();
+		int listNum, n=0;
+		Iterator<Fashion2DTO> it = list.iterator();
+		while(it.hasNext()){
+			Fashion2DTO dto = it.next();
+			listNum = dataCount- (start+n-1);
+			dto.setListNum(listNum);
+			try{
+				Date sDate = format.parse(dto.getCreated());
+				gap=(date.getTime()-sDate.getTime())/(60*60*1000); //60*60*1000 -> 1시간    .getTime -> 밀리세컨드
+				dto.setCreated(dto.getCreated().substring(0,10));
+				dto.setGap(gap);
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			n++;
+		}
+		
+		String query = "";
+		if (searchValue.length() != 0) {
+			// 검색인 경우 검색값 인코딩
+			searchValue = URLEncoder.encode(searchValue, "utf-8");
+			query = "searchKey=" + searchKey + "&searchValue=" + searchValue;
+		}
+
+		// 페이징 처리
+		String listUrl = cp + "/fashion/Fashion2.do";
+		String articleUrl = cp + "/fashion/Fashion2_article.do?page=" + current_page;
+		if (query.length() != 0) {
+			listUrl += "?" + query;
+			articleUrl += "&" + query;
+		}
+		
+		String paging = util.paging(current_page, total_page, listUrl);
+
+		// 포워딩할 JSP로 넘길 속성
+		req.setAttribute("list", list);
+		req.setAttribute("listNotice", listNotice);
+		req.setAttribute("page", current_page);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("articleUrl", articleUrl);
+		req.setAttribute("paging", paging);
+
+		// JSP로 포워딩
+		String path = "/WEB-INF/views/fashion/list2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_createdForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글쓰기 폼
+		req.setAttribute("mode", "created");
+		String path = "/WEB-INF/views/fashion/created2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 게시물 저장
+		String cp = req.getContextPath();
+
+		Fashion2DAO dao = new Fashion2DAO();
+		Fashion2DTO dto = new Fashion2DTO();
+
+		String encType="utf-8";
+		int maxFilesize=10*1024*1024;//10MB
+		
+		MultipartRequest mreq=new MultipartRequest(
+	    		req, pathname, maxFilesize, encType,
+	    		new DefaultFileRenamePolicy()
+	    		);
+		
+		// mem_Id는 세션에 저장된 정보
+		dto.setMem_Id(info.getMem_Id());
+
+		// 파라미터
+		if(info.getMem_Id().equals("admin") && mreq.getParameter("notice")!=null){
+				dto.setCategory("공지");
+				dto.setNotice(Integer.parseInt(mreq.getParameter("notice")));
+		}else{
+			dto.setCategory(mreq.getParameter("category"));
+		}
+		dto.setSubject(mreq.getParameter("subject"));
+		dto.setContent(mreq.getParameter("content"));
+
+		dao.insertFashion2(dto, "created");
+		// 포인트 추가(게시판은 10 / 답글은 5 / 댓글은 2)
+		MemberDAO mdao = new MemberDAO();
+		mdao.updatePoint(info.getMem_Id(), 10);
+
+		resp.sendRedirect(cp + "/fashion/Fashion2.do");
+	}
+
+	private void Fashion2_article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 게시물 보기
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		String page = req.getParameter("page");
+		String searchKey = req.getParameter("searchKey");
+		String searchValue = req.getParameter("searchValue");
+		if (searchKey == null) {
+			searchKey = "subject";
+			searchValue = "";
+		}
+
+		searchValue = URLDecoder.decode(searchValue, "utf-8");
+
+		// 조회수 증가
+		dao.updateHitCount(bbs_num);
+
+		// 게시물 가져오기
+		Fashion2DTO dto = dao.readFashion2(bbs_num);
+		
+		if (dto == null) { // 게시물이 없으면 다시 리스트로
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+
+		int countLike = dao.countLike(bbs_num);
+		//dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+		// 이전글 다음글
+		Fashion2DTO preReadDto = dao.preReadFashion2(dto.getGroupNum(), dto.getOrderNo(), searchKey, searchValue);
+		Fashion2DTO nextReadDto = dao.nextReadFashion2(dto.getGroupNum(), dto.getOrderNo(), searchKey, searchValue);
+		// 리스트나 이전글/다음글에서 사용할 파라미터
+		String query = "page=" + page;
+		if (searchValue.length() != 0) {
+			query += "&searchKey=" + searchKey + "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");
+		}
+
+		// JSP로 전달할 속성
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("query", query);
+		req.setAttribute("preReadDto", preReadDto);
+		req.setAttribute("nextReadDto", nextReadDto);
+		req.setAttribute("countLike", countLike);
+
+		// 포워딩
+		String path = "/WEB-INF/views/fashion/article2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 폼
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+		String page = req.getParameter("page");
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		Fashion2DTO dto = dao.readFashion2(bbs_num);
+
+		if (dto == null) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+
+		// 게시물을 올린 사용자가 아니면
+		if (!dto.getMem_Id().equals(info.getMem_Id())) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("mode", "update");
+
+		String path = "/WEB-INF/views/fashion/created2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 완료
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+
+		String encType="utf-8";
+		int maxFilesize=10*1024*1024;//10MB
+		
+	    MultipartRequest mreq=new MultipartRequest(
+	    		req, pathname, maxFilesize, encType,
+	    		new DefaultFileRenamePolicy()
+	    		);
+		
+	    String page = mreq.getParameter("page");
+		/*if (req.getMethod().equalsIgnoreCase("GET")) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}*/
+
+		Fashion2DTO dto = new Fashion2DTO();
+		dto.setBbs_num(Integer.parseInt(mreq.getParameter("bbs_num")));
+		dto.setSubject(mreq.getParameter("subject"));
+		dto.setContent(mreq.getParameter("content"));
+
+		dao.updateFashion2(dto, info.getMem_Id());
+
+		resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+	}
+
+	private void Fashion2_replyForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 답변폼
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		String page = req.getParameter("page");
+		Fashion2DTO dto = dao.readFashion2(bbs_num);
+		if (dto == null) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+		dto.setContent("[" + dto.getSubject() + "] 에 대한 답변입니다.\n");
+
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("mode", "reply");
+
+		String path = "/WEB-INF/views/fashion/created2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_replySubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 답변완료
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+		// 파라미터:subject,content,page,
+		// groupNum(부의 groupNum),
+		// depth(부의 depth),orderNo(부의 orderNo),
+		// parent(부의 bbs_num)
+		
+		String encType="utf-8";
+		int maxFilesize=10*1024*1024;//10MB
+		
+		MultipartRequest mreq=new MultipartRequest(
+	    		req, pathname, maxFilesize, encType,
+	    		new DefaultFileRenamePolicy()
+	    		);
+		String page = mreq.getParameter("page");
+
+		Fashion2DTO dto = new Fashion2DTO();
+		dto.setMem_Id(info.getMem_Id());
+
+		dto.setSubject(mreq.getParameter("subject"));
+		dto.setContent(mreq.getParameter("content"));
+		dto.setGroupNum(Integer.parseInt(mreq.getParameter("groupNum")));
+		dto.setDepth(Integer.parseInt(mreq.getParameter("depth")));
+		dto.setOrderNo(Integer.parseInt(mreq.getParameter("orderNo")));
+		dto.setParent(Integer.parseInt(mreq.getParameter("parent")));
+
+		dao.insertFashion2(dto, "reply");
+		// 포인트 추가(게시판은 10 / 답글은 5 / 댓글은 2)
+		MemberDAO mdao = new MemberDAO();
+		mdao.updatePoint(info.getMem_Id(), 5);
+
+		resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+	}
+
+	private void Fashion2_delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 삭제
+		String cp = req.getContextPath();
+		Fashion2DAO dao = new Fashion2DAO();
+
+		String page = req.getParameter("page");
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		Fashion2DTO dto = dao.readFashion2(bbs_num);
+
+		if (dto == null) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+
+		// 게시물을 올린 사용자나 admin이 아니면
+		if (!dto.getMem_Id().equals(info.getMem_Id()) && !info.getMem_Id().equals("admin")) {
+			resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+			return;
+		}
+
+		dao.deleteFashion2(bbs_num);
+		resp.sendRedirect(cp + "/fashion/Fashion2.do?page=" + page);
+	}
+	
+	private void Fashion2_likecnt(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Fashion2DAO dao = new Fashion2DAO();
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		
+		int countLike=dao.countLike(bbs_num);
+		JSONObject job=new JSONObject();
+		job.put("countLike", countLike);
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out=resp.getWriter();
+		out.print(job.toString());
+	}
+
+	private void Fashion2_like(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Fashion2DAO dao = new Fashion2DAO();
+		
+		String state="false";
+		if(info==null) {
+			state="loginFail";
+		} else {
+			int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+
+			int result=dao.updateLike(bbs_num, info.getMem_Id());
+			if(result==1)
+				state="true";
+		}
+		
+		JSONObject job=new JSONObject();
+		job.put("state", state);
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out=resp.getWriter();
+		out.print(job.toString());
+	}
+	
+	private void Fashion2_listReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 리플 리스트(AJAX:TEXT)
+		Fashion2DAO dao = new Fashion2DAO();
+		MyUtil util = new MyUtil();
+		
+		int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+		String pageNo = req.getParameter("pageNo");
+		int current_page = 1;
+		if (pageNo != null)
+			current_page = Integer.parseInt(pageNo);
+
+		int numPerPage = 5;
+		int total_page = 0;
+		int replyCount = 0;
+
+		replyCount = dao.dataCountReply(bbs_num);
+		total_page = util.pageCount(numPerPage, replyCount);
+		if (current_page > total_page)
+			current_page = total_page;
+
+		int start = (current_page - 1) * numPerPage + 1;
+		int end = current_page * numPerPage;
+
+		// 리스트에 출력할 데이터
+		List<Fashion2ReplyDTO> listReply = dao.listReply(bbs_num, start, end);
+
+		// 엔터를 <br>
+		Iterator<Fashion2ReplyDTO> it = listReply.iterator();
+		while (it.hasNext()) {
+			Fashion2ReplyDTO dto = it.next();
+			dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+		}
+
+		// 페이징처리(인수2개 짜리 js로 처리)
+		String paging = util.paging(current_page, total_page);
+
+		req.setAttribute("listReply", listReply);
+		req.setAttribute("pageNo", current_page);
+		req.setAttribute("replyCount", replyCount);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("paging", paging);
+		
+
+		// 포워딩
+		String path = "/WEB-INF/views/fashion/listReply2.jsp";
+		forward(req, resp, path);
+	}
+
+	private void Fashion2_insertReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 리플 저장(JSON)
+		Fashion2DAO dao = new Fashion2DAO();
+		
+		String state="false";
+		if(info==null) {
+			state="loginFail";
+		} else {
+			int bbs_num = Integer.parseInt(req.getParameter("bbs_num"));
+			Fashion2ReplyDTO rdto = new Fashion2ReplyDTO();
+			rdto.setBbs_num(bbs_num);
+			rdto.setMem_Id(info.getMem_Id());
+			rdto.setContent(req.getParameter("content"));
+
+			int result=dao.insertReply(rdto);
+			if(result==1){
+				state="true";
+				// 포인트 추가(게시판은 10 / 답글은 5 / 댓글은 2)
+				MemberDAO mdao = new MemberDAO();
+				mdao.updatePoint(info.getMem_Id(), 2);
+			}
+		}
+		
+		JSONObject job=new JSONObject();
+		job.put("state", state);
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out=resp.getWriter();
+		out.print(job.toString());
+	}
+	
+	protected void Fashion2_deleteReply(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	      Fashion2DAO dao = new Fashion2DAO();      
+	      
+	      int reply_num = Integer.parseInt(req.getParameter("reply_num"));
+	      
+	      String state="false";
+	      if(info==null) {
+	         state="loginFail";
+	      } else {
+	         int result=dao.deleteReply(reply_num, info.getMem_Id());
+	         if(result==1) {
+	            state="true";
+	         }
+	      }      
+	      
+	      JSONObject job=new JSONObject();
+	      job.put("state", state);
+	      
+	      resp.setContentType("text/html;charset=utf-8");
+	      PrintWriter out=resp.getWriter();
+	      out.print(job.toString());
 	}
 }
